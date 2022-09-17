303ba086d637b7ca4db5fa6708b82f1edbe9c195
saulkaique <saul.silva@etec.sp.gov.br>
2022-09-17 09:30:19 -0300

    bom esse e o comando ja terminado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string nome, sobrenome;
+
+Console.Write("Digite seu nome: ");
+
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+
+sobrenome = Console.ReadLine()!;
+
+
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catalogo: {sobrenome.ToUpper()}, {nome}");
